6433ef2efc4681423df6c0a7ed32226afdb2d8d7
ace-brown <kazem_abdi@yahoo.com>
2025-05-20 17:46:34 +0200

    no commit message

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 
 // Middleware to handle CORS (Cross-Origin Resource Sharing)
-const allowedOrigins = ['https://www.khayamsana.sbs', 'http://localhost:3000', 'http://localhost:5173'];
+const allowedOrigins = ['https://www.vadi.click', 'http://localhost:3000', 'http://localhost:5173'];
 
 app.use((req, res, next) => {
     const origin = req.headers.origin;
