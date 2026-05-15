ed20ba07b61426e47d90da34b0e433f3f148ab46
ace-brown <kazem_abdi@yahoo.com>
2026-05-15 17:36:22 +0200

    Revert "Add readme"
    This reverts commit e64549ac64207498493858e61e64c85004133701.

--- server/index.js
+++ server/index.js
@@ -1,24 +1,24 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const path = require("path");
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
 
-const usersRoutes = require("./src/routes/users-routes");
-const mobileTariffRoutes = require("./src/routes/internet/mobile-tariffs-routes");
-const homeTariffRoutes = require("./src/routes/internet/home-tariffs-routes");
-const menSalonPlanRoutes = require("./src/routes/aesthetic/men-salons-routes");
-const womenSalonPlanRoutes = require("./src/routes/aesthetic/women-salons-routes");
-const autoRepairRoutes = require("./src/routes/vehicle/auto-repair-routes");
-const motorcycleRepairRoutes = require("./src/routes/vehicle/motorcycle-repair-routes");
-const applianceRepairRoutes = require("./src/routes/electronics/appliance-repair-routes");
-const mobileRepairRoutes = require("./src/routes/PM/mobile-repair-routes");
-const computerRepairRoutes = require("./src/routes/PM/computer-repair-routes");
-const gardenSuppliesRoutes = require("./src/routes/agriculture/garden-supplies-routes");
-const animalBasedProdRoutes = require("./src/routes/agriculture/animal-based-products-routes");
-const dentalServiceRoutes = require("./src/routes/healthcare/dental-services-routes");
-const bookstoresRoutes = require("./src/routes/education/bookstores-routes");
-const eduCenterRoutes = require("./src/routes/education/education-centers-routes");
-const downloadRoutes = require("./src/routes/download-routes");
-const HttpError = require("./src/models/http-error");
+const usersRoutes = require('./src/routes/users-routes')
+const mobileTariffRoutes = require('./src/routes/internet/mobile-tariffs-routes')
+const homeTariffRoutes = require('./src/routes/internet/home-tariffs-routes')
+const menSalonPlanRoutes = require('./src/routes/aesthetic/men-salons-routes')
+const womenSalonPlanRoutes = require('./src/routes/aesthetic/women-salons-routes')
+const autoRepairRoutes = require('./src/routes/vehicle/auto-repair-routes')
+const motorcycleRepairRoutes = require('./src/routes/vehicle/motorcycle-repair-routes')
+const applianceRepairRoutes = require('./src/routes/electronics/appliance-repair-routes')
+const mobileRepairRoutes = require('./src/routes/PM/mobile-repair-routes')
+const computerRepairRoutes = require('./src/routes/PM/computer-repair-routes')
+const gardenSuppliesRoutes = require('./src/routes/agriculture/garden-supplies-routes')
+const animalBasedProdRoutes = require('./src/routes/agriculture/animal-based-products-routes')
+const dentalServiceRoutes = require('./src/routes/healthcare/dental-services-routes')
+const bookstoresRoutes = require('./src/routes/education/bookstores-routes')
+const eduCenterRoutes = require('./src/routes/education/education-centers-routes')
+const downloadRoutes = require('./src/routes/download-routes')
+const HttpError = require('./src/models/http-error')
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -27,108 +27,93 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 
 // Middleware to handle CORS (Cross-Origin Resource Sharing)
-const allowedOrigins = [
-  "https://www.vadi.click",
-  "http://localhost:3000",
-  "http://localhost:5173",
-];
+const allowedOrigins = ['https://www.vadi.click', 'http://localhost:3000', 'http://localhost:5173'];
 
 app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS",
-  );
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(204); // Preflight response
-  }
-  next();
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(204); // Preflight response
+    }
+    next();
 });
 
-app.use(
-  "/uploads/images",
-  express.static(path.join(__dirname, "uploads", "images")),
-);
+app.use('/uploads/images', express.static(path.join(__dirname, 'uploads', 'images')));
 
 // Download Routes
-app.use("/api/download", downloadRoutes);
+app.use('/api/download', downloadRoutes);
 
 // Moible Tariff routes
-app.use("/api/internet/mobile-tariffs", mobileTariffRoutes);
+app.use('/api/internet/mobile-tariffs', mobileTariffRoutes);
 
 // Home Tariff routes
-app.use("/api/internet/home-tariffs", homeTariffRoutes);
+app.use('/api/internet/home-tariffs', homeTariffRoutes);
 
 // Men Salon Plan routes
-app.use("/api/aesthetic/men-salon", menSalonPlanRoutes);
+app.use('/api/aesthetic/men-salon', menSalonPlanRoutes);
 
 // Women Salon Plan routes
-app.use("/api/aesthetic/women-salon", womenSalonPlanRoutes);
+app.use('/api/aesthetic/women-salon', womenSalonPlanRoutes);
 
 // Auto Repair routes
-app.use("/api/vehicle/auto-repair", autoRepairRoutes);
+app.use('/api/vehicle/auto-repair', autoRepairRoutes);
 
 // Motorcycle Repair routes
-app.use("/api/vehicle/motorcycle-repair", motorcycleRepairRoutes);
+app.use('/api/vehicle/motorcycle-repair', motorcycleRepairRoutes);
 
 // Appliance Repair routes
-app.use("/api/electronics/appliance-repair", applianceRepairRoutes);
+app.use('/api/electronics/appliance-repair', applianceRepairRoutes);
 
 // Mobile Repair routes
-app.use("/api/pc-mobile/mobile-repair", mobileRepairRoutes);
+app.use('/api/pc-mobile/mobile-repair', mobileRepairRoutes);
 
 // Computer Repair routes
-app.use("/api/pc-mobile/computer-repair", computerRepairRoutes);
+app.use('/api/pc-mobile/computer-repair', computerRepairRoutes);
 
 // Garden Supplies routes
-app.use("/api/agriculture/garden-supplies", gardenSuppliesRoutes);
+app.use('/api/agriculture/garden-supplies', gardenSuppliesRoutes);
 
 // Animal-based products routes
-app.use("/api/agriculture/animal-based-products", animalBasedProdRoutes);
+app.use('/api/agriculture/animal-based-products', animalBasedProdRoutes);
 
 // Dental Services routes
-app.use("/api/healthcare/dental", dentalServiceRoutes);
+app.use('/api/healthcare/dental', dentalServiceRoutes);
 
 // Bookstores routes
-app.use("/api/education/bookstores", bookstoresRoutes);
+app.use('/api/education/bookstores', bookstoresRoutes);
 
 // Education Center routes
-app.use("/api/education/centers", eduCenterRoutes);
+app.use('/api/education/centers', eduCenterRoutes);
 
 // Users routes
-app.use("/api/users", usersRoutes);
+app.use('/api/users', usersRoutes);
 
-// Middleware to handle wrong routes
+// Middleware to handle wrong routes 
 app.use(function (req, res, next) {
-  throw new HttpError("Could not find this route.", 404);
-});
+    throw new HttpError('Could not find this route.', 404)
+})
 
 // Middleware to handle errors after they have been sent
 app.use(function (error, req, res, next) {
-  if (res.headerSent) {
-    return next(error);
-  }
-  res.status(error.code || 500);
-  res.json({ message: error.message || "An unknown error occurred!" });
+    if (res.headerSent) {
+        return next(error);
+    }
+    res.status(error.code || 500)
+    res.json({ message: error.message || 'An unknown error occurred!' });
 });
 
 // Connect to MongoDB
 mongoose
-  .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.SUFFIX}.${process.env.AFTER_SUFFIX}.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=${process.env.APP_NAME}`,
-  )
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+    .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.SUFFIX}.${process.env.AFTER_SUFFIX}.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=${process.env.APP_NAME}`)
+    .then(function () {
+        app.listen(port, () => console.log(`Server is running at port ${port}`))
+    })
+    .catch((err) => { console.log(err) })
 
-module.exports = app;
